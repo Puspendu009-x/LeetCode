8553e02d221e9118c22eada334e78bdf47f4377d
Puspendu Sahoo <puspendusahoo15@gmail.com>
2026-08-08 23:01:54 +0530

    Time: 42 ms (69.76%) | Memory: 10.4 MB (83.68%) - LeetSync

---
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int n = p.size();
+        int m = s.size();
+
+        vector<bool>prev(m+1,false),curr(m+1,0);
+
+        prev[0] = true;
+        for(int j=1;j<=m;j++){
+            prev[j]=false;
+        }
+
+        for(int i=1;i<=n;i++){
+            bool flag = true;
+            for(int ii=1;ii<=i;ii++){
+                if(p[ii-1]!='*'){
+                    flag = false;
+                    break;
+                }
+            }
+            curr[0]=flag;
+            for(int j=1;j<=m;j++){
+                if(p[i-1]==s[j-1] || p[i-1]=='?'){
+                    curr[j]=prev[j-1];
+                }
+                else if(p[i-1]=='*'){
+                    curr[j] = prev[j]|curr[j-1];
+                }
+                else{
+                    curr[j]= false;
+                }
+            }
+            prev =curr;
+        }
+        return prev[m];
+    }
+};
