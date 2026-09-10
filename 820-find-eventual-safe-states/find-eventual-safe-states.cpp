2a612030096c6d93da7da71289191ac3f35dc0c3
Puspendu Sahoo <puspendusahoo15@gmail.com>
2026-09-10 20:50:22 +0530

    Time: 61 ms (12.44%) | Memory: 66.3 MB (36.43%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,44 +1,37 @@
 class Solution {
-private:
-    bool dfs(int node, vector<int>& vis, vector<int>& check,
-             vector<int>& pathvis, vector<vector<int>>& graph) {
-        vis[node] = 1;
-        check[node] = 0;
-        pathvis[node] = 1;
-
-        for (auto it : graph[node]) {
-            if (!vis[it]) {
-                if (dfs(it, vis, check, pathvis, graph))
-                return true;
-            }
-
-            else {
-                if (pathvis[it]) {
-                    return true;
-                }
-            }
-        }
-        check[node] = 1;
-        pathvis[node] = 0;
-        return false;
-    };
-
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int n = graph.size();
-        vector<int> vis(n, 0);
-        vector<int> pathvis(n, 0);
-        vector<int> check(n, 0);
-        vector<int> safenode;
-        for (int i = 0; i < n; i++) {
-            if (!vis[i])
-                dfs(i, vis, check, pathvis, graph);
+        int n=graph.size();
+        vector<vector<int>>gRev(n);
+        vector<int>indegree(n,0);
+        for(int i=0;i<n;i++){
+            for(auto it:graph[i]){
+                gRev[it].push_back(i);
+                indegree[i]++;
+            }
         }
 
-        for (int i = 0; i < n; i++) {
-            if (check[i])
-                safenode.push_back(i);
+        queue<int>q;
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0)
+            q.push(i);
         }
-        return safenode;
+
+        vector<int>safe_node;
+
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            safe_node.push_back(node);
+
+            for(auto it:gRev[node]){
+                indegree[it]--;
+                if(indegree[it]==0)
+                q.push(it);
+            }
+        }
+
+        sort(safe_node.begin(),safe_node.end());
+        return safe_node;
     }
 };
