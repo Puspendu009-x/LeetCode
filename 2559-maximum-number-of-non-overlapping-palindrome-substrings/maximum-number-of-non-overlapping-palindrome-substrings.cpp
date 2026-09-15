9fde4dcf9edaf8dd83e3ef0ae08972a77d5ea1ca
Puspendu Sahoo <puspendusahoo15@gmail.com>
2026-09-15 15:44:33 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (91.36%) - LeetSync

--- 2559-maximum-number-of-non-overlapping-palindrome-substrings/maximum-number-of-non-overlapping-palindrome-substrings.cpp
+++ 2559-maximum-number-of-non-overlapping-palindrome-substrings/maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -1,26 +1,40 @@
 class Solution {
+private:
+    // Optimized to check in-place without creating or reversing substrings
+    bool isPalin(const string& s, int l, int r) {
+        while (l < r) {
+            if (s[l] != s[r]) return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+
 public:
     int maxPalindromes(string s, int k) {
         int n = s.size();
+        int ans = 0;
+        
         if (k == 1) return n;
 
-        int res = 0;
-
-        for (int i = 0; i <= n - k;) {
-            int step = 1;
-
-            for (int d : {k, k + 1}) {
-                if (i + d <= n && equal(s.begin() + i, s.begin() + i + (d >> 1),
-                                        s.rbegin() + (n - (i + d)))) {
-                    step = d;
-                    res++;
-                    break;
-                }
+        int i = 0;
+        while (i <= n - k) {
+            // Greedy Choice 1: Can we make a palindrome of exactly length k?
+            if (isPalin(s, i, i + k - 1)) {
+                ans++;
+                i += k; // Jump entirely past this palindrome to prevent overlaps
+            } 
+            // Greedy Choice 2: Can we make a palindrome of exactly length k+1?
+            else if (i + k < n && isPalin(s, i, i + k)) {
+                ans++;
+                i += k + 1; // Jump entirely past this palindrome
+            } 
+            else {
+                // If neither length k nor k+1 works starting here, slide the window by 1
+                i++;
             }
-
-            i += step;
         }
-
-        return res;
+        
+        return ans;
     }
 };
